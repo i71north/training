e0f30c22642be27e2f53eacc1dca8c263a7e802b
i71north <i71north@yahoo.com>
2017-07-07 15:48:25 -0400

    Worked on helloWorld and userInput projects

---
+++ HelloDemo/HelloDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HelloDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            //Console.ReadKey();
+
+            Console.WriteLine("We are here to become developers");
+            Console.WriteLine("Today is Friday");
+            //Console.WriteLine("Busy weekend ahead");
+
+            //Console.ReadKey();
+            Console.WriteLine("We will go to lunch soon");
+            Console.Write("now");
+            Console.Write("today");
+            Console.ReadKey();
+
+
+
+
+            //int 5a = 10;
+            //  5 int =x;
+
+
+
+        }
+    }
+}
---
+++ UserInput/UserInput/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " How are you today?");
+            //string concatenation
+            Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("You are an {0} year old woman", age);
+            //varible dropping into place
+            Console.WriteLine(" {0} You are a {1} year old woman am I right {2}", name, age, age);
+            Console.ReadLine();
+
+            Console.WriteLine("Are you going out tonight:");
+            string goingout = Console.ReadLine();
+
+            //string interpolation
+            Console.WriteLine($"My name is {name} I am {age} and {goingout} of course");
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
