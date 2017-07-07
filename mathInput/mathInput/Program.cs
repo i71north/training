9b838e75289d34ab41b92d12fb275e0611ef34e1
i71north <i71north@yahoo.com>
2017-07-07 16:24:59 -0400

    Worked on the mathInput project

---
+++ mathInput/mathInput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Give me the first # to add");
+            string firstNum = Console.ReadLine();
+            int firstN = Convert.ToInt16(firstNum);
+
+            Console.WriteLine("Give me the second # you want to add");
+            string secondNum = Console.ReadLine();
+            int secondN = Convert.ToInt32(secondNum);
+
+            double sum = secondN + firstN;
+            Console.WriteLine($"your first Number was {firstNum} and your second number was {secondNum} the sum is {sum}");
+            Console.ReadLine();
+         
+
+        }
+    }
+}
