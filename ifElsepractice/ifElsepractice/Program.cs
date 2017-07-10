f66541533be0e2afe71431b510be6ed3a3e4a047
i71north <i71north@yahoo.com>
2017-07-10 16:34:38 -0400

    worked on if statements

---
+++ ifElsepractice/ifElsepractice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifElsepractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color?");
+            string favcolor = Console.ReadLine().ToUpper();
+
+            if (favcolor == "RED")
+            {
+                Console.WriteLine("red is good");
+            }
+            else
+            {
+                Console.WriteLine("Whatever!");
+
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
