511b429d9f42f563c2abfbfd49cfe15e16b0512c
i71north <i71north@yahoo.com>
2017-07-10 15:57:21 -0400

    Worked on interpolation

---
+++ consolePractice1/consolePractice1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolePractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name?");
+            //string dude = Console.ReadLine();
+
+            //Console.WriteLine($"My name is {dude}");
+            //Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+
+            Console.WriteLine($"I am {age} years old.");
+            Console.ReadLine();
+
+            Console.WriteLine("What is the name of your street name?");
+            string street = Console.ReadLine();
+
+            Console.WriteLine("What is email address?");
+            string email = Console.ReadLine();
+
+            Console.WriteLine($" My age is {age}, my street name is {street}, my email address is {email}.");
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ consolePractice2/consolePractice2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolePractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Console.WriteLine("What day of the week is it?");
+         string day = Console.ReadLine();
+
+            Console.WriteLine($"Today is {day}");
+            Console.ReadLine();
+
+         Console.WriteLine("What month are we currently in?");
+         string month = Console.ReadLine();
+
+            Console.WriteLine($"We are currently in {month}");
+            Console.ReadLine();
+
+         Console.WriteLine("What is today's date?");
+         string date = Console.ReadLine();
+
+            Console.WriteLine($"Today's date is the {date}");
+            Console.ReadLine();
+
+         Console.WriteLine("What year is it?");
+         string year = Console.ReadLine();
+
+            Console.WriteLine($"It is {year}");
+            Console.ReadLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
