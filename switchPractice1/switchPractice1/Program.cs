2e303c341357153c4c6c2255f47554bcdeb9695c
i71north <i71north@yahoo.com>
2017-07-12 15:32:58 -0400

    worked on switches

---
+++ switchPractice1/switchPractice1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite color");
+            string color = Console.ReadLine();
+
+            switch(color)
+            {
+                case "red":
+                    Console.WriteLine("Red is an ok color");
+                    break;
+
+                default:
+                    Console.WriteLine("We don't understand your answer.");
+                    break;
+            }
+        }
+    }
+}
