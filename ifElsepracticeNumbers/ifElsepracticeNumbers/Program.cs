13c1ae2c94183133831ade4ccc28092309f53928
i71north <i71north@yahoo.com>
2017-07-12 12:24:30 -0400

    worked on if else, and, or

---
+++ ifElsepracticeNumbers/ifElsepracticeNumbers/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ifElsepracticeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Give me a number");
+            //string number1 = Console.ReadLine();
+            //int num1 = Convert.ToInt32(number1);
+
+            //Console.WriteLine("Give me a second number");
+            //string number2 = Console.ReadLine();
+            //int num2 = Convert.ToInt32(number2);
+
+            //if (num1 < num2)
+            //{
+            //    Console.WriteLine($"{number1} is less than {number2}");
+            //}
+            //else if (num1 > num2)
+            //{
+            //    Console.WriteLine($"Your first number {num1} is greater than your second number {num2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Your first number {num1} is equal to your second number of {num2}");
+            //}
+            //Console.ReadLine();
+
+
+            //Console.WriteLine("How old are you?");
+            //string age1 = Console.ReadLine();
+            //int num1 = Convert.ToInt32(age1);
+
+            //Console.WriteLine("How old is your sister?");
+            //string age2 = Console.ReadLine();
+            //int num2 = Convert.ToInt32(age2);
+
+            //if (num1 < num2)
+            //{
+            //    Console.WriteLine($"Because you are {age1} and she is {age2} you are the younger sister.");
+            //}
+            //else if (num1 > num2)
+            //{
+            //    Console.WriteLine($"Because you are {age1} and she is {age2} you are the older sister.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"You and your sister are both {age1}. Wow, are you twins!");
+            //}
+            //Console.ReadLine();
+
+
+
+
+            //Console.WriteLine("Give me a first number");
+            //int number1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Give me a second number");
+            //int number2 = int.Parse(Console.ReadLine());
+
+            //if (number1 > number2)
+            //{
+            //    Console.WriteLine($"Your number {number1} is greater than your number of {number2}");
+            //}
+            //else if (number1 < number2)
+            //{
+            //    Console.WriteLine($"Your number {number1} is less than your number of {number2}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Your numer {number1} is equal to your number of {number2}");
+            //}
+            //Console.ReadKey();
+
+
+            WriteLine("Enter a number:");
+            double var1 = Convert.ToDouble(ReadLine());
+            WriteLine("Enter another number:");
+            double var2 = Convert.ToDouble(ReadLine());
+            WriteLine("Enter a 3rd number:");
+            decimal dec3 = Convert.ToDecimal(ReadLine());
+
+
+
+
+        }
+
+
+
+    }
+}
+
--- ternaryPractice1/ternaryPractice1/Program.cs
+++ ternaryPractice1/ternaryPractice1/Program.cs
@@ -10,13 +10,41 @@ namespace ternaryPractice1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your favorite sport");
-            string sport = Console.ReadLine().ToLower();
+            //Console.WriteLine("What is your favorite sport");
+            //string sport = Console.ReadLine().ToLower();
 
-            string favSport = (sport == "hockey" || sport == "football" || sport == "golf") ? "Great Choice" : "Bad Choice";
+            //string favSport = (sport == "hockey" || sport == "football" || sport == "golf") ? "Great Choice" : "Bad Choice";
 
-            Console.WriteLine(favSport);
+            //Console.WriteLine(favSport);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is a food that you like?");
+            //string food = Console.ReadLine().ToLower();
+            //Console.WriteLine("What is the second kind of food you like?");
+            //string food2 = Console.ReadLine().ToLower();
+            //Console.WriteLine("Name a third food?");
+            //string food3 = Console.ReadLine().ToLower();
+
+
+            //string foodChoice = (food == "chicken" && food2 == "shrimp" && food3 == "fish") ? "Great Choice" : "Bad Choice";
+            //Console.WriteLine(foodChoice);
+            //Console.ReadLine();
+
+            Console.WriteLine("What is the first vowel in the alphabet");
+            string vowel1 = Console.ReadLine().ToLower();
+            Console.WriteLine("What is the second vowel in the alphabet");
+            string vowel2 = Console.ReadLine().ToLower();
+            Console.WriteLine("What is the thrid vowel in the alphabet");
+            string vowel3 = Console.ReadLine().ToLower();
+            Console.WriteLine("What is the forth vowel in the alphabet");
+            string vowel4 = Console.ReadLine().ToLower();
+            Console.WriteLine("What is the fifth vowel in the alphabet");
+            string vowel5 = Console.ReadLine().ToLower();
+            string answer = (vowel1 == "a" && vowel2 == "e" && vowel3 == "i" && vowel4 == "o" && vowel5 == "u") ? "Good Job!!!" : "Sorry, try again";
+            Console.WriteLine(answer);
             Console.ReadLine();
+
         }
+
     }
 }
