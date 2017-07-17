5c983151fd655f5ed111d602c8fc842b67aadeae
i71north <i71north@yahoo.com>
2017-07-17 12:34:55 -0400

    learning loops

---
+++ loopPractice1/loopPractice1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0; define a vaule for i, which is 0
+            //i < 10; = says that i is less than 10
+            //i++; ++ means to increment the value of i 
+            // arrays = collection of liked typed varibles
+            // arrays of fixed size
+            // array sizes cannot be changed onced created
+            // arrays are 0 based
+
+            int[] numbers = { 2, 4, 88, 11, 55, 100, 165 };
+            string[] stars = { "Beyonce", "Rhianna", "Janet Jackson", "Madoanna" };
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"The value of i is {i}");
+            }
+            Console.ReadLine();
+
+            for (int k = 0; k < stars.Length; ++k)
+            //.Length covers any amount of varibles w/out having to know or enter the exact # of vars 
+            {
+                Console.WriteLine($"The stars are " + (stars[k]));
+            }
+            Console.ReadLine();
+
+            for (int f = 0; f < numbers.Length; ++f)
+            {
+                Console.WriteLine("The stars are " + (numbers[f]));
+            }
+            Console.ReadLine();
+
+
+            for (int d = 0; d < numbers.Length; ++d)
+            {
+                Array.Reverse(numbers);
+                Console.WriteLine(numbers[d]);
+            }
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ loopPractice2/loopPractice2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] animals = { "Dog", "Cat", "Fox", "Bird", "Duck" };
+          
+            for (int a = 0; a < animals.Length; ++a)
+            {
+                Console.WriteLine("I saw a " + (animals[a]) + " in the park");
+            }
+            Console.ReadLine();
+
+            int[] nums = new int[4];
+            nums[0] = 10;
+            nums[1] = 20;
+            nums[2] = 30;
+            nums[3] = 40;
+
+            for (int n = 0; n < nums.Length; ++n)
+            {
+                Console.WriteLine("The values of the array nums is " + (nums[n]));
+            }
+            Console.ReadLine();
+
+            char[] letters = { 'v', 'i', 'n', 'c', 'e' };
+            //use ' when using a char (or one single letter)
+            for(int m = 0; m < letters.Length; ++m)
+            {
+                Console.WriteLine("The values of the letters array are "+ (letters[m]));
+            }
+            Console.ReadLine();
+        }
+
+        
+    }
+}
