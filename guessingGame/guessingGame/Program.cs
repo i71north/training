f8fefdb7969d81d307f1ef16432669b7baecd1ba
i71north <i71north@yahoo.com>
2017-07-18 12:29:34 -0400

    practice with static and non static methods

---
+++ guessingGame/guessingGame/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //Random rnd = new Random();
+
+            Vacation v = new Vacation();
+            Console.WriteLine("Hello World!");
+
+            p.Tuesday();
+            p.Working();
+            Lunch();
+            Talking();
+            v.Bahamas();
+            v.Cancun();
+      
+            Console.WriteLine("I'm baaaaack in the main method!");
+            Console.ReadKey();
+        }
+        public void Working()
+        {
+            Console.WriteLine("Hey...I'm working over here!");
+        }
+        public static void Talking()
+        {
+            Console.WriteLine("Doing alot of talking today");
+        }
+        public void Tuesday()
+        {
+            Console.WriteLine("Tuesday's are the best!");
+            Console.ReadLine();
+        }
+        public static void Lunch()
+        {
+            Console.WriteLine("I love it when it's lunchtime!!!!!!!!!");
+        }
+    }
+}
---
+++ guessingGame/guessingGame/Vacation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessingGame
+{
+    class Vacation
+    {
+        public void Cancun()
+        {
+            Console.WriteLine("I like Senor Frogs");
+        }
+
+        public void Bahamas()
+        {
+            Console.WriteLine("Sandal & Atlantis are both here");
+        }
+    }
+}
