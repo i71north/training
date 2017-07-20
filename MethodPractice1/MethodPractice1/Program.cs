f50d93b8653165da5f265fff9b988cf9f75ee6aa
i71north <i71north@yahoo.com>
2017-07-20 11:59:07 -0400

    practicing calling static and non static methods

---
+++ MethodPractice1/MethodPractice1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resturants r = new Resturants();
+            Fashion f = new Fashion();
+
+
+
+            Console.WriteLine("This is the main methodlanding page.");
+
+            r.Chipotle();
+            Console.ReadLine();
+
+            f.Pants();
+
+
+            Console.ReadLine();
+        }
+
+
+        public static void Addition()
+        {
+
+
+
+        }
+
+        public static void Multiplication()
+        {
+
+
+        }
+
+            
+    }
+
+    class Fashion
+    {
+        public void Shirts()
+        {
+            Console.WriteLine("Buttonups");
+            Console.WriteLine("V Neck");
+            Console.WriteLine("Henleys");
+        }
+
+        public void Pants()
+        
+{
+            
+Console.WriteLine("Bellbottoms");
+            Console.WriteLine("Sweats");
+            Console.WriteLine("Petalpushers");
+
+        }
+
+        public void Shoes()
+        {
+
+
+        }
+
+        
+
+    }
+
+
+}
---
+++ MethodPractice1/MethodPractice1/Resturants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPractice1
+{
+    class Resturants
+    {
+        public static void FamousDaves()
+        {
+            Fashion fash = new Fashion();
+            Resturants r = new Resturants();
+            
+            fash.Pants();
+            fash.Shirts();
+            r.Chipotle();
+            
+        }
+        public void Chipotle()
+
+        {
+            Console.WriteLine("We make buritos");
+        }
+        public void Benihana()
+        {
+            Console.WriteLine("We cook the food right in front of you!");
+        }
+    }
+}
