cd85de42a3d00ee2dd15cfe65f837f3b6ada64aa
i71north <i71north@yahoo.com>
2017-07-24 16:23:43 -0400

    passing reference methods

---
+++ MethodPassingReference1/MethodPassingReference1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPassingReference1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Console.WriteLine($"In the main method x = {x}.");
+            DisplayValueParameter(x);
+            Console.WriteLine($"In the main method the value of x is now {x}.");
+            Console.ReadLine();
+        }
+
+        private static void DisplayValueParameter(int x)
+        {
+            x = 1000;
+            Console.WriteLine($"We are in the Display Parmeter Method \n Where x = {x}.");
+        }
+    }
+}
---
+++ MethodPassingReference2-Strings/MethodPassingReference2-Strings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodPassingReference2_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Player 1, what is your first name?");
+            string player1 = Console.ReadLine();
+            nameChange(player1);
+            Console.WriteLine($"In the main method... the name of player 1 is {player1}.");
+            nameTransform(ref player1);
+            Console.WriteLine($"Inside of the very very main method the value of player 1 is {player1}");
+
+            Console.ReadLine();
+        }
+
+        public static void nameChange(string originalPlayer1)
+        {
+            originalPlayer1 = "new player 1";
+            Console.WriteLine($"Inside of the name change method, the name of Player 1 \n is {originalPlayer1}");
+        }
+
+        public static void nameTransform(ref string player1)
+        {
+            player1 = "I am king of the world";
+            Console.WriteLine($"Today {player1}");
+        }
+
+
+
+
+
+
+    }   
+
+}
+
